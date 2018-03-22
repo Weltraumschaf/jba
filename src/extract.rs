be796029d4bcabf94419705e70a362e0da59fb1f
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-03-22 09:51:23 +0100

    Add some doc about constant pool structure

--- src/extract.rs
+++ src/extract.rs
@@ -51,11 +51,43 @@ pub fn extract_major_version(mut class_file: &File) -> String {
         rdr.read_u16::<BigEndian>().unwrap())
 }
 
+/// Extracts the constant pool.
+///
+/// All constant_pool table entries have the following general format:
+/// ```ignore
+/// cp_info {
+///     u1 tag;
+///     u1 info[];
+/// }
+/// ```
+/// Constant pool tags:
+/// - CONSTANT_Class = 7
+/// - CONSTANT_Fieldref = 9
+/// - CONSTANT_Methodref = 10
+/// - CONSTANT_InterfaceMethodref = 11
+/// - CONSTANT_String = 8
+/// - CONSTANT_Integer = 3
+/// - CONSTANT_Float = 4
+/// - CONSTANT_Long = 5
+/// - CONSTANT_Double = 6
+/// - CONSTANT_NameAndType = 12
+/// - CONSTANT_Utf8 = 1
+/// - CONSTANT_MethodHandle = 15
+/// - CONSTANT_MethodType = 16
+/// - CONSTANT_InvokeDynamic = 18
 pub fn extract_constant_pool(mut class_file: &File) -> String {
     let mut buffer = [0; 2];
     class_file.read(&mut buffer[..]).unwrap();
     let mut rdr = Cursor::new(&buffer);
-    format!("{} (d{})",
+    let number_of_constants = rdr.read_u16::<BigEndian>().unwrap();
+    let mut format_buffer = String::new();
+    format_buffer.push_str(&format!("{} (d{})\n",
         format_entry("constant_pool_count:", &buffer),
-        rdr.read_u16::<BigEndian>().unwrap())
+        number_of_constants));
+
+    for constant_number in 1..number_of_constants + 1 {
+        format_buffer.push_str(&format!("constant #{}\n", constant_number));
+    }
+
+    format_buffer
 }
